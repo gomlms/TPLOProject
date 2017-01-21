60f7aae3844712ac23d972f497f1a9654363b1dd
Max Sidebotham <gomlms@maxs-air.home>
2017-01-20 21:37:07 -0500

    Added New Classes based off of the meal app and also started coding part of the table view

---
+++ TPLO Pre-Op Analysis/Procedure.swift
@@ -0,0 +1,56 @@
+//
+//  Procedure.swift
+//  TPLO Pre-Op Analysis
+//
+//  Created by Max Sidebotham on 1/20/17.
+//  Copyright © 2017 Preda Studios. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+class Procedure: NSObject, NSCoding {
+
+    //MARK: Properties
+    var name : String
+    var dateOfProcedure : String
+    var radiograph : UIImage?
+    
+    //MARK: Initialization
+    init?(n:String, r:UIImage?, d:String){
+        guard !n.isEmpty else {
+            return nil
+        }
+        
+        name = n
+        radiograph = r
+        dateOfProcedure = d
+    }
+    
+    
+    //MARK: NSCoding
+    public func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(dateOfProcedure, forKey: PropertyKey.dateOfProcedure)
+        aCoder.encode(radiograph, forKey: PropertyKey.radiograph)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder){
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else{
+            os_log("Unable to decode name of radiograph", log: OSLog.default, type:.debug)
+            return nil
+        }
+        
+        let dateOfProcedure = aDecoder.decodeObject(forKey: PropertyKey.dateOfProcedure) as! String
+        let radiograph = aDecoder.decodeObject(forKey: PropertyKey.radiograph) as? UIImage
+    
+        self.init(n: name, r: radiograph, d: dateOfProcedure)
+    }
+    
+    //MARK: Types
+    struct PropertyKey{
+        static let name = "name"
+        static let dateOfProcedure = "dateOfProcedure"
+        static let radiograph = "radiograph"
+    }
+}
---
+++ TPLO Pre-Op Analysis/ProcedureCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProcedureCell.swift
+//  TPLO Pre-Op Analysis
+//
+//  Created by Max Sidebotham on 1/20/17.
+//  Copyright © 2017 Preda Studios. All rights reserved.
+//
+
+import UIKit
+
+class ProcedureCell: UITableViewCell {
+
+}
