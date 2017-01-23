fe122f181dfa294ad097e881bc885450c5ab79b7
Max Sidebotham <gomlms@wireless-10-104-139-146.umd.edu>
2017-01-23 13:42:43 -0500

    Starting transfer of Procedure files between views

--- TPLO Pre-Op Analysis/FirstPropertiesViewController.swift
+++ TPLO Pre-Op Analysis/FirstPropertiesViewController.swift
@@ -59,7 +59,7 @@ class FirstPropertiesViewController: UIViewController, UITextFieldDelegate, UIIm
             fatalError("The Properties View Controller is not inside a navigation controller")
         }
     }
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let button = sender as? UIBarButtonItem, button === nextButton else {
@@ -76,21 +76,63 @@ class FirstPropertiesViewController: UIViewController, UITextFieldDelegate, UIIm
         dateFormatter.dateStyle = DateFormatter.Style.long
         let currentDate = dateFormatter.string(from: date)
         
-        
         procedure = Procedure(n: name, r: photo, d: currentDate)
         
+        os_log("Not in if statement", log: OSLog.default, type: .debug)
+        
+        if segue.identifier == "Continue" {
+            guard let procedureDataViewController = segue.destination as? RelativeDistanceViewController else {
+                fatalError("Unexpected destination: \(segue.destination)")
+            }
+            procedureDataViewController.radiographImage.image = imageSelector.image
+            os_log("In if statement", log:OSLog.default, type: .debug)
+        }
         super.prepare(for: segue, sender: sender)
     }
     
     //MARK: Actions
     @IBAction func selectImageFromLibrary(_ sender: UITapGestureRecognizer) {
+        //Hide Keyboard
         nameTextField.resignFirstResponder()
+        
+        //UIImagePickerController allows user to choose photo from storage
+        let imagePickerController = UIImagePickerController()
+        
+        //Only allow Images to be chosen
+        imagePickerController.sourceType = .photoLibrary
+        
+        //Make Sure view controller is notified when photo is chosen
+        imagePickerController.delegate = self
+        
+        present(imagePickerController, animated: true, completion: nil)
     }
     
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        //The info dictionary may contain multiple representations of the image. You want to use the original
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else{
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        //Set Photo Image to display the selected image view
+        imageSelector.image = selectedImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+
 
     //MARK: Private Methods
     private func updateNextButtonState() {
         let text = nameTextField.text ?? ""
-        nextButton.isEnabled = !text.isEmpty
+        
+        if text == "Enter title here" {
+            nextButton.isEnabled = false
+        } else {
+            nextButton.isEnabled = true
+        }
     }
 }
---
+++ TPLO Pre-Op Analysis/RelativeDistanceViewController.swift
@@ -0,0 +1,47 @@
+//
+//  RelativeDistanceViewController.swift
+//  TPLO Pre-Op Analysis
+//
+//  Created by Max Sidebotham on 1/22/17.
+//  Copyright © 2017 Preda Studios. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+class RelativeDistanceViewController: UIViewController {
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var radiographImage: UIImageView!
+    @IBOutlet weak var nextButton: UIBarButtonItem!
+    
+    var pointOneCreated = false
+    var pointTwoCreated = false
+    
+    
+    //MARK: Actions
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: Private Methods
+    private func updateNextButtonState() {
+        if pointOneCreated && pointTwoCreated {
+            nextButton.isEnabled = true
+        } else {
+            nextButton.isEnabled = false
+        }
+    }
+
+}
