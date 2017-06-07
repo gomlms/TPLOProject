c2dbb540f4eb081424f2ed1ff5c664abdf821455
Max Sidebotham <gomlms@Maxs-MacBook-Air.local>
2017-06-07 14:09:46 -0400

    Changes

--- TPLO Pre-Op Analysis/SetSawbladeViewController.swift
+++ TPLO Pre-Op Analysis/SetSawbladeViewController.swift
@@ -15,16 +15,20 @@ class SetSawbladeViewController: UIViewController {
     var point1Button = UIView()
     var point2Button = UIView()
     var point3Button = UIView()
+    var point31Button = UIView()
     var point4Button = UIView()
     var point5Button = UIView()
     var confirmSelection = UIView()
+    var point8Button = UIView()
     
     var button1Recog = UITapGestureRecognizer()
     var button2Recog = UITapGestureRecognizer()
     var button3Recog = UITapGestureRecognizer()
+    var button31Recog = UITapGestureRecognizer()
     var button4Recog = UITapGestureRecognizer()
     var button5Recog = UITapGestureRecognizer()
     var confirmRecog = UITapGestureRecognizer()
+    var button8Recog = UITapGestureRecognizer()
     
     var selectedColor : UIColor = UIColor(red:0.00, green:0.74, blue:0.89, alpha:0.7)
     var unselectedColor : UIColor = UIColor(red:0.00, green:0.32, blue:0.61, alpha:0.7)
@@ -41,41 +45,28 @@ class SetSawbladeViewController: UIViewController {
         super.viewDidLoad()
 
         let screenWidth = UIScreen.main.bounds.width
-        let buttonWidth = screenWidth / 3.0
-        let dotRadius = buttonWidth / 3
-        let dotPosX = buttonWidth / 3
-        let dotPosY = menuView.frame.height / 12.0
+        let buttonWidth = screenWidth / 4.0
         
         point1Button.frame = CGRect(x: 0, y: 0, width: buttonWidth, height: menuView.frame.height / 2)
         
-        let dot1Image = UIImageView(image: #imageLiteral(resourceName: "dot1"))
-        dot1Image.frame = CGRect(x: dotPosX, y: dotPosY, width: dotRadius, height: dotRadius)
-        dot1Image.contentMode = UIViewContentMode.scaleAspectFit
-        
-        let dot1Label = UILabel(frame: CGRect(x: buttonWidth / 10, y: dotPosY + dotRadius + menuView.frame.height / 24, width: buttonWidth * 4 / 5, height: 60))
+        let dot1Label = UILabel(frame: CGRect(x: buttonWidth / 10, y: buttonWidth / 3, width: buttonWidth * 4 / 5, height: 60))
         dot1Label.lineBreakMode = NSLineBreakMode.byWordWrapping
         dot1Label.numberOfLines = 0
-        dot1Label.font = UIFont(name:"Open Sans", size: 16)
+        dot1Label.font = UIFont(name:"Open Sans", size: 20)
         dot1Label.textColor = UIColor.white
         dot1Label.textAlignment = .center
-        dot1Label.text = "Intercondylar\n Eminence"
+        dot1Label.text = "12mm\n Sawblade"
         
         point1Button.layer.borderColor = UIColor.gray.cgColor
         point1Button.layer.borderWidth = 2.0
         point1Button.layer.shadowRadius = 10.0
-        
-        point1Button.addSubview(dot1Image)
         point1Button.addSubview(dot1Label)
         
         menuView.addSubview(point1Button)
         
         point2Button.frame = CGRect(x: buttonWidth, y: 0, width: buttonWidth, height: menuView.frame.height / 2)
         
-        let dot2Image = UIImageView(image: #imageLiteral(resourceName: "dot2"))
-        dot2Image.frame = CGRect(x: dotPosX, y: dotPosY, width: dotRadius, height: dotRadius)
-        dot2Image.contentMode = UIViewContentMode.scaleAspectFit
-        
-        let dot2Label = UILabel(frame: CGRect(x: buttonWidth / 10, y: dotPosY + dotRadius + menuView.frame.height / 24, width: buttonWidth * 4 / 5, height: 60))
+        let dot2Label = UILabel(frame: CGRect(x: buttonWidth / 10, y: buttonWidth / 3, width: buttonWidth * 4 / 5, height: 60))
         dot2Label.lineBreakMode = NSLineBreakMode.byWordWrapping
         dot2Label.numberOfLines = 0
         dot2Label.font = UIFont(name:"Open Sans", size: 16)
@@ -87,18 +78,13 @@ class SetSawbladeViewController: UIViewController {
         point2Button.layer.borderWidth = 2.0
         point2Button.layer.shadowRadius = 10.0
         
-        point2Button.addSubview(dot2Image)
         point2Button.addSubview(dot2Label)
         
         menuView.addSubview(point2Button)
         
         point3Button.frame = CGRect(x: 2 * buttonWidth, y: 0, width: buttonWidth, height: menuView.frame.height / 2)
         
-        let dot3Image = UIImageView(image: #imageLiteral(resourceName: "dot3"))
-        dot3Image.frame = CGRect(x: dotPosX, y: dotPosY, width: dotRadius, height: dotRadius)
-        dot3Image.contentMode = UIViewContentMode.scaleAspectFit
-        
-        let dot3Label = UILabel(frame: CGRect(x: buttonWidth / 10, y: dotPosY + dotRadius + menuView.frame.height / 24, width: buttonWidth * 4 / 5, height: 60))
+        let dot3Label = UILabel(frame: CGRect(x: buttonWidth / 10, y: buttonWidth / 3, width: buttonWidth * 4 / 5, height: 60))
         dot3Label.lineBreakMode = NSLineBreakMode.byWordWrapping
         dot3Label.numberOfLines = 0
         dot3Label.font = UIFont(name:"Open Sans", size: 16)
@@ -110,18 +96,32 @@ class SetSawbladeViewController: UIViewController {
         point3Button.layer.borderWidth = 2.0
         point3Button.layer.shadowRadius = 10.0
         
-        point3Button.addSubview(dot3Image)
         point3Button.addSubview(dot3Label)
         
         menuView.addSubview(point3Button)
         
+        point31Button.frame = CGRect(x: 3 * buttonWidth, y: 0, width: buttonWidth, height: menuView.frame.height / 2)
+        
+        let dot31Label = UILabel(frame: CGRect(x: buttonWidth / 10, y: buttonWidth / 3, width: buttonWidth * 4 / 5, height: 60))
+        dot31Label.lineBreakMode = NSLineBreakMode.byWordWrapping
+        dot31Label.numberOfLines = 0
+        dot31Label.font = UIFont(name:"Open Sans", size: 16)
+        dot31Label.textColor = UIColor.white
+        dot31Label.textAlignment = .center
+        dot31Label.text = "Tibial Plateau\nCranial"
+        
+        point31Button.layer.borderColor = UIColor.gray.cgColor
+        point31Button.layer.borderWidth = 2.0
+        point31Button.layer.shadowRadius = 10.0
+        
+        point31Button.addSubview(dot31Label)
+        
+        menuView.addSubview(point31Button)
+        
+        
         point4Button.frame = CGRect(x: 0, y: menuView.frame.height / 2, width: buttonWidth, height: menuView.frame.height / 2)
         
-        let dot4Image = UIImageView(image: #imageLiteral(resourceName: "dot4"))
-        dot4Image.frame = CGRect(x: dotPosX, y: dotPosY, width: dotRadius, height: dotRadius)
-        dot4Image.contentMode = UIViewContentMode.scaleAspectFit
-        
-        let dot4Label = UILabel(frame: CGRect(x: buttonWidth / 10, y: dotPosY + dotRadius + menuView.frame.height / 24, width: buttonWidth * 4 / 5, height: 60))
+        let dot4Label = UILabel(frame: CGRect(x: buttonWidth / 10, y: buttonWidth / 3, width: buttonWidth * 4 / 5, height: 60))
         dot4Label.lineBreakMode = NSLineBreakMode.byWordWrapping
         dot4Label.numberOfLines = 0
         dot4Label.font = UIFont(name:"Open Sans", size: 16)
@@ -133,18 +133,13 @@ class SetSawbladeViewController: UIViewController {
         point4Button.layer.borderWidth = 2.0
         point4Button.layer.shadowRadius = 10.0
         
-        point4Button.addSubview(dot4Image)
         point4Button.addSubview(dot4Label)
         
         menuView.addSubview(point4Button)
         
         point5Button.frame = CGRect(x: buttonWidth, y: menuView.frame.height / 2, width: buttonWidth, height: menuView.frame.height / 2)
         
-        let dot5Image = UIImageView(image: #imageLiteral(resourceName: "dot5"))
-        dot5Image.frame = CGRect(x: dotPosX, y: dotPosY, width: dotRadius, height: dotRadius)
-        dot5Image.contentMode = UIViewContentMode.scaleAspectFit
-        
-        let dot5Label = UILabel(frame: CGRect(x: buttonWidth / 10, y: dotPosY + dotRadius + menuView.frame.height / 24, width: buttonWidth * 4 / 5, height: 60))
+        let dot5Label = UILabel(frame: CGRect(x: buttonWidth / 10, y: buttonWidth / 3, width: buttonWidth * 4 / 5, height: 60))
         dot5Label.lineBreakMode = NSLineBreakMode.byWordWrapping
         dot5Label.numberOfLines = 0
         dot5Label.font = UIFont(name:"Open Sans", size: 16)
@@ -155,19 +150,14 @@ class SetSawbladeViewController: UIViewController {
         point5Button.layer.borderColor = UIColor.gray.cgColor
         point5Button.layer.borderWidth = 2.0
         point5Button.layer.shadowRadius = 10.0
-        
-        point5Button.addSubview(dot5Image)
+    
         point5Button.addSubview(dot5Label)
         
         menuView.addSubview(point5Button)
         
         confirmSelection.frame = CGRect(x: 2 * buttonWidth, y: menuView.frame.height / 2, width: buttonWidth, height: menuView.frame.height / 2)
         
-        let confirmImage = UIImageView(image: #imageLiteral(resourceName: "CheckButtonBlue"))
-        confirmImage.frame = CGRect(x: dotPosX, y: dotPosY, width: dotRadius, height: dotRadius)
-        confirmImage.contentMode = UIViewContentMode.scaleAspectFit
-        
-        let confirmLabel = UILabel(frame: CGRect(x: buttonWidth / 10, y: dotPosY + dotRadius + menuView.frame.height / 24, width: buttonWidth * 4 / 5, height: 60))
+        let confirmLabel = UILabel(frame: CGRect(x: buttonWidth / 10, y: buttonWidth / 3, width: buttonWidth * 4 / 5, height: 60))
         confirmLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
         confirmLabel.numberOfLines = 0
         confirmLabel.font = UIFont(name:"Open Sans", size: 16)
@@ -179,10 +169,28 @@ class SetSawbladeViewController: UIViewController {
         confirmSelection.layer.borderWidth = 2.0
         confirmSelection.layer.shadowRadius = 10.0
         
-        confirmSelection.addSubview(confirmImage)
         confirmSelection.addSubview(confirmLabel)
         
         menuView.addSubview(confirmSelection)
+        
+        point8Button.frame = CGRect(x: 3 * buttonWidth, y: menuView.frame.height / 2, width: buttonWidth, height: menuView.frame.height / 2)
+        
+        let dot8Label = UILabel(frame: CGRect(x: buttonWidth / 10, y: buttonWidth / 3, width: buttonWidth * 4 / 5, height: 60))
+        dot8Label.lineBreakMode = NSLineBreakMode.byWordWrapping
+        dot8Label.numberOfLines = 0
+        dot8Label.font = UIFont(name:"Open Sans", size: 16)
+        dot8Label.textColor = UIColor.white
+        dot8Label.textAlignment = .center
+        dot8Label.text = "Proximal\nTubercle"
+        
+        point8Button.layer.borderColor = UIColor.gray.cgColor
+        point8Button.layer.borderWidth = 2.0
+        point8Button.layer.shadowRadius = 10.0
+        
+        point8Button.addSubview(dot8Label)
+        
+        menuView.addSubview(point8Button)
+
         
         menuView.isUserInteractionEnabled = true
         point1Button.isUserInteractionEnabled = true
@@ -193,28 +201,66 @@ class SetSawbladeViewController: UIViewController {
         confirmSelection.isUserInteractionEnabled = false
         
         
-        button1Recog.addTarget(self, action: #selector(SelectFirstFourPointsViewController.selectPoint1(_:)))
-        button2Recog.addTarget(self, action: #selector(SelectFirstFourPointsViewController.selectPoint2(_:)))
-        button3Recog.addTarget(self, action: #selector(SelectFirstFourPointsViewController.selectPoint3(_:)))
-        button4Recog.addTarget(self, action: #selector(SelectFirstFourPointsViewController.selectPoint4(_:)))
-        button5Recog.addTarget(self, action: #selector(SelectFirstFourPointsViewController.selectPoint5(_:)))
-        confirmRecog.addTarget(self, action: #selector(SelectFirstFourPointsViewController.nextMenu(_:)))
+        button1Recog.addTarget(self, action: #selector(SetSawbladeViewController.Blade12(_:)))
+        button2Recog.addTarget(self, action: #selector(SetSawbladeViewController.Blade15(_:)))
+        button3Recog.addTarget(self, action: #selector(SetSawbladeViewController.Blade18(_:)))
+        button3Recog.addTarget(self, action: #selector(SetSawbladeViewController.Blade21(_:)))
+        button4Recog.addTarget(self, action: #selector(SetSawbladeViewController.Blade24(_:)))
+        button5Recog.addTarget(self, action: #selector(SetSawbladeViewController.Blade27(_:)))
+        confirmRecog.addTarget(self, action: #selector(SetSawbladeViewController.Blade30(_:)))
+        button8Recog.addTarget(self, action: #selector(SetSawbladeViewController.Blade33(_:)))
         
         point1Button.addGestureRecognizer(button1Recog)
         point2Button.addGestureRecognizer(button2Recog)
         point3Button.addGestureRecognizer(button3Recog)
+        point31Button.addGestureRecognizer(button31Recog)
         point4Button.addGestureRecognizer(button4Recog)
         point5Button.addGestureRecognizer(button5Recog)
         confirmSelection.addGestureRecognizer(confirmRecog)
+        point8Button.addGestureRecognizer(button8Recog)
         
         point1Button.backgroundColor = unselectedColor
         point2Button.backgroundColor = unselectedColor
         point3Button.backgroundColor = unselectedColor
         point4Button.backgroundColor = unselectedColor
         point5Button.backgroundColor = unselectedColor
+        point31Button.backgroundColor = unselectedColor
+        point8Button.backgroundColor = unselectedColor
         confirmSelection.backgroundColor = greyColor
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func Blade12(_ sender: Any){
+    
+    }
+    
+    @IBAction func Blade15(_ sender: Any){
+        
+    }
+    
+    @IBAction func Blade18(_ sender: Any){
+        
+    }
+    
+    @IBAction func Blade21(_ sender: Any){
+        
+    }
+    
+    @IBAction func Blade24(_ sender: Any){
+        
+    }
+    
+    @IBAction func Blade27(_ sender: Any){
+        
+    }
+    
+    @IBAction func Blade30(_ sender: Any){
+        
+    }
+    
+    @IBAction func Blade33(_ sender: Any){
+        
     }
 
     override func didReceiveMemoryWarning() {
