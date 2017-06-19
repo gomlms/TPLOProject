66eca28ea33c31cf1809af032a2e692191d82f3d
Max Sidebotham <gomlms@Maxs-MacBook-Air.local>
2017-06-19 11:56:39 -0400

    adds

--- TPLO Pre-Op Analysis/FirstPropertiesViewController.swift
+++ TPLO Pre-Op Analysis/FirstPropertiesViewController.swift
@@ -61,6 +61,7 @@ class FirstPropertiesViewController: UIViewController, UITextFieldDelegate, UIIm
         
         updateNextButtonState()
         // Do any additional setup after loading the view.
+        nextButton.isEnabled = false
         
         nameTextField.delegate = self
         
@@ -211,11 +212,98 @@ class FirstPropertiesViewController: UIViewController, UITextFieldDelegate, UIIm
         dismiss(animated: true, completion: nil)
     }
     
+    func fixOrientation(image: UIImage) -> UIImage? {
+        guard let cgImage = image.cgImage else {
+            return nil
+        }
+        
+        if image.imageOrientation == UIImageOrientation.up {
+            return image
+        }
+        
+        let width  = image.size.width
+        let height = image.size.height
+        
+        var transform = CGAffineTransform.identity
+        
+        switch image.imageOrientation {
+        case .down, .downMirrored:
+            transform = transform.translatedBy(x: width, y: height)
+            transform = transform.rotated(by: CGFloat.pi)
+            
+        case .left, .leftMirrored:
+            transform = transform.translatedBy(x: width, y: 0)
+            transform = transform.rotated(by: 0.5*CGFloat.pi)
+            
+        case .right, .rightMirrored:
+            transform = transform.translatedBy(x: 0, y: height)
+            transform = transform.rotated(by: -0.5*CGFloat.pi)
+            
+        case .up, .upMirrored:
+            break
+        }
+        
+        switch image.imageOrientation {
+        case .upMirrored, .downMirrored:
+            transform = transform.translatedBy(x: width, y: 0)
+            transform = transform.scaledBy(x: -1, y: 1)
+            
+        case .leftMirrored, .rightMirrored:
+            transform = transform.translatedBy(x: height, y: 0)
+            transform = transform.scaledBy(x: -1, y: 1)
+            
+        default:
+            break;
+        }
+        
+        // Now we draw the underlying CGImage into a new context, applying the transform
+        // calculated above.
+        guard let colorSpace = cgImage.colorSpace else {
+            return nil
+        }
+        
+        guard let context = CGContext(
+            data: nil,
+            width: Int(width),
+            height: Int(height),
+            bitsPerComponent: cgImage.bitsPerComponent,
+            bytesPerRow: 0,
+            space: colorSpace,
+            bitmapInfo: UInt32(cgImage.bitmapInfo.rawValue)
+            ) else {
+                return nil
+        }
+        
+        context.concatenate(transform);
+        
+        switch image.imageOrientation {
+            
+        case .left, .leftMirrored, .right, .rightMirrored:
+            // Grr...
+            context.draw(cgImage, in: CGRect(x: 0, y: 0, width: height, height: width))
+            
+        default:
+            context.draw(cgImage, in: CGRect(x: 0, y: 0, width: width, height: height))
+        }
+        
+        // And now we just create a new UIImage from the drawing context
+        guard let newCGImg = context.makeImage() else {
+            return nil
+        }
+        
+        let img = UIImage(cgImage: newCGImg)
+        
+        return img;
+    }
+
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         //The info dictionary may contain multiple representations of the image. You want to use the original
-        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else{
+        guard var selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else{
             fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
         }
+        
+        selectedImage = fixOrientation(image: selectedImage)!
         
         //Set Photo Image to display the selected image view
         radiographImage = selectedImage
@@ -304,12 +392,6 @@ class FirstPropertiesViewController: UIViewController, UITextFieldDelegate, UIIm
     
     //MARK: Private Methods
     private func updateNextButtonState() {
-        let text = nameTextField.text ?? ""
         
-        if text == "Enter title here..." || desChosen == false{
-            nextButton.isEnabled = false
-        } else {
-            nextButton.isEnabled = true
-        }
     }
 }
